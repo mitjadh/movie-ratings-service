5b1a8ad740d950f33b375d3b3b75dda1769a4fc3
mitjadh <88079193+mitjadh@users.noreply.github.com>
2021-09-16 12:38:33 +0530

    Add files via upload

---
+++ src/main/java/io/springboot/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,34 @@
+package io.springboot.ratingsdataservice.resources;
+
+import io.springboot.ratingsdataservice.model.Rating;
+import io.springboot.ratingsdataservice.model.UserRating;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/users/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        List<Rating> ratings=Arrays.asList(
+        		new Rating("1234",4),
+        		new Rating("5678",3));
+        
+		UserRating userRating=new UserRating();
+		userRating.setUserRating(ratings);
+		return userRating;
+		
+    }
+
+}
